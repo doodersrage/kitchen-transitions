fa4c1cc6449b14b5a57fa931da3756e03fdbc947
Robert McDowell <rob@studiocenter.com>
2014-06-08 16:20:42 -0400

    finished design

--- script/home.js
+++ script/home.js
@@ -1,6 +1,14 @@
+var homeSlide;
+var slideIdx = 1;
+var slideImgs = [];
+var prevIdx = 0;
 $(function(){
 	// pull center block to fill full vertical
 	$('.container').height($(window).height());
+	
+	$(window).resize(function(){
+		$('.container').height($(window).height());
+	});
 	
 	// fade out on click
 	$('nav a').bind('click', function(e){
@@ -14,4 +22,14 @@ $(function(){
 			window.location = clkHref;
 		});
 	});
+		
+	// init home background gallery
+	$.vegas('slideshow', {
+		delay: 7000,
+		backgrounds:[
+			{ src:'img/home-bg.jpg', fade: 4000 },
+			{ src:'img/home-bg1.jpg', fade: 4000 },
+			{ src:'img/home-bg3.jpg', fade: 4000 }
+		]
+	})();
 });
