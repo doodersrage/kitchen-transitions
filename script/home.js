3e7aa86f61521030b957ef3c3c219dea12266f09
Robert McDowell <rob@studiocenter.com>
2014-06-08 14:09:56 -0400

    Layout nearly complete.
    Everything completed except for home page slider and image gallery page.

--- script/home.js
+++ script/home.js
@@ -1,4 +1,17 @@
 $(function(){
 	// pull center block to fill full vertical
 	$('.container').height($(window).height());
+	
+	// fade out on click
+	$('nav a').bind('click', function(e){
+		// prevent default link click action
+		e.preventDefault();
+		
+		// gather clicked link href
+		clkHref = $(this).attr('href');
+		
+		$('.container').fadeOut('slow',function(){
+			window.location = clkHref;
+		});
+	});
 });
--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
 $(function(){
+	// launch slider
+	$('.headslide').nivoSlider({
+		pauseTime: 7000,
+		directionNav: false,
+		controlNav: false,
+		pauseOnHover: false,
+		effect:'fade'
+	});
 });
